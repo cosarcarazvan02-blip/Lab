52882750c71ae55d9aaa85c40844180572dd09ab
Cosarca Dumitru-Razvan <cosarcarazvan02@gmail.com>
2026-09-18 10:00:06 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -4,21 +4,52 @@ class Main{
     public static void main(String args[]){
         Scanner scn = new Scanner(System.in);
 
-        while (true) {
-            System.out.println("Introduceti un cnp de 6 cifre :");
-            int x = scn.nextInt();
+        System.out.println("Introduceti un numar de 4 cifre:");
+        int x = scn.nextInt();
 
-            int anul = x / 10000;
-            int luna = (x / 100) % 100;
-            int ziua = x % 100;
+        int ultima = x % 10;
+        int trei = (x / 10) % 10;
+        int doi = (x / 100) % 10;
+        int prima = x / 1000;
+        int suma = prima + doi + trei + ultima;
 
-            if (luna >= 1 && luna <= 12 && ziua >= 1 && ziua <= 31) {
-                System.out.println("Data nasterii este: " + anul + "." + luna + "." + ziua);
-                break;
-            } else {
-                System.out.println("Date invalida! Verifica luna, anul si ziua. Incearca din nou!\n");
+        int optiune;
+        do{
+            System.out.println("1.Afisare paritate ultima cifra");
+            System.out.println("2.Afisare paritate prima cifra.");
+            System.out.println("3.Afisare suma cifrelor");
+            System.out.println("0.Iesire");
+
+            optiune = scn.nextInt();
+
+            switch(optiune){
+                case 1:
+                    if(ultima % 2 == 0){
+                        System.out.println("Cifra para este:" + ultima);
+                    }else{
+                        System.out.println("Numarul nu este par!");
+                    }
+                    break;
+
+                case 2:
+                    if(prima % 2 == 0){
+                        System.out.println("Cidra para este:" + prima);
+                    }else{
+                        System.out.println("Numarul nu este par!");
+                    }
+                    break;
+
+                case 3:
+                    System.out.println("Suma cifrelor este:" + suma);
+                    break;
+
+                case 0:
+                    System.out.println("Goodbye!");
+                    break;
+
+                default:
+                    System.out.println("Optiune invalida!");
             }
-        }
+        }while(optiune != 0);
     }
 }
-
